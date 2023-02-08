d748eeb4d2417342e33566358a73412233e25779
amogusdevlol <104919677+amogusdevlol@users.noreply.github.com>
2023-02-08 19:56:33 +0800

    fixes

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ var logger = require('./logger');
         var { execSync } = require('child_process');
             const fs = require('fs-extra')
                 try {
-            logger('Cập Nhật Đã Lỗi Tiến Hành Xóa Package', "[ FCA - SP ]");
+            logger('Update Failed Proceed to Delete Package', "[ FCA - SP ]");
                 execSync('npm cache clean --force', { stdio: 'ignore'})
                     await new Promise(resolve => setTimeout(resolve, 2*1000))
                         fs.removeSync('../fca-horizon-remake');
                             // why stdio is not studio :v 
                         await new Promise(resolve => setTimeout(resolve, 2*1000))
-                    execSync('npm i fca-horizon-remake@latest', { stdio: 'ignore'})
-                logger("Đã Thành Công - Tiến Hành Restart", "[ FCA - SP ]");
+                    execSync('npm i fca-sus@latest', { stdio: 'ignore'})
+                logger("Succeeded - Restarting", "[ FCA - SP ]");
             process.exit(1);
         }
         catch (e) {
-            logger("Đã Bị Lỗi Hãy Nhập Vào Console Mã Sau Đây Để Fix !", "[ FCA - SP ]");
-                logger("rmdir ./node_modules/fca-horizon-remake && npm i fca-horizon-remake@latest && npm start", '[ FCA - SP ]');
-            logger("Hãy Copy Hết Những Chữ Trên, Cần Làm Đúng 100% Nếu Ko File Bạn Sẽ Bay Màu ✨", "[ FCA - SP ]")
+            logger("Error Please Enter The Following Code In Console To Fix !", "[ FCA - SP ]");
+                logger("rmdir ./node_modules/fca-sus && npm i fca-sus@latest && npm start", '[ FCA - SP ]');
+            logger("Please Copy All The Above Words, Need To Do It 100% Correctly Otherwise Your File Will Be Discolored ✨", "[ FCA - SP ]")
         process.exit(0);
         }
     }
@@ -31,7 +31,7 @@ var logger = require('./logger');
                     }
                 }
             catch (e) {
-        logger.onLogger('Đã Xảy Ra Lỗi Khi Cố Gửi Lỗi Đến Server', '[ FB - API ]'," #FF0000")
+        logger.onLogger('An Error Occurred When Trying To Send Error To Server', '[ FB - API ]'," #FF0000")
     } 
             // <= End Submit The Error To The Api => //
 }
